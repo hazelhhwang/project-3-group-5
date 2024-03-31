e101c3c131644915a97d015b25d0599285068cae
Ashritha Surisetti <ashritha.surisetti@gmail.com>
2024-03-31 23:14:56 +1100

    adding sql file

--- restaurant_reviews_updated.sql
+++ restaurant_reviews_updated.sql
@@ -4,7 +4,7 @@ DROP TABLE restaurant_reviews;
 --creating a table
 CREATE TABLE restaurant_reviews (
 	name VARCHAR(200),
-	cusinetype VARCHAR(50),
+	cuisinetype VARCHAR(50),
 	rankingPosition INT,
 	priceLevel VARCHAR(15),
 	category VARCHAR(30),
@@ -19,21 +19,34 @@ CREATE TABLE restaurant_reviews (
 -- View table columns and datatypes
 SELECT * FROM restaurant_reviews;
 
---Top 3 restaurants based on their ranking position
-SELECT name, rankingPosition, address, numberOfReviews
-FROM restaurant_reviews
-WHERE rankingPosition IN (1, 2, 3)
-ORDER BY numberOfReviews DESC;
 
 --Avg rating of each cusine and no.of restaurants in Melbourne
-SELECT cusinetype, ROUND(AVG(rating)::numeric,1) AS avg_rating, COUNT(*) AS num_restaurants
+SELECT cuisinetype, ROUND(AVG(rating)::numeric,1) AS avg_rating, COUNT(*) AS num_restaurants
 FROM restaurant_reviews
-GROUP BY cusinetype
+GROUP BY cuisinetype
 ORDER BY avg_rating DESC, num_restaurants DESC;
+
+--Average price Level of each cusine type
+SELECT cuisinetype,
+       CASE 
+           WHEN AVG(CASE WHEN priceLevel = '$' THEN 1 ELSE 0 END) > 0.5 THEN '$'
+           WHEN AVG(CASE WHEN priceLevel = '$$' THEN 1 ELSE 0 END) > 0.5 THEN '$$'
+           WHEN AVG(CASE WHEN priceLevel = '$$ - $$$' THEN 1 ELSE 0 END) > 0.5 THEN '$$ - $$$'
+           WHEN AVG(CASE WHEN priceLevel = '$$$$' THEN 1 ELSE 0 END) > 0.5 THEN '$$$$'
+           ELSE MAX(priceLevel) --Using the max function when there isn't a clear majority of one particular price level for a cuisine type
+		   END AS avg_price_level
+FROM restaurant_reviews
+GROUP BY cuisinetype;
+
+--No.of restaurants in each price level
+SELECT priceLevel, COUNT(*) AS num_restaurants
+FROM restaurant_reviews
+GROUP BY priceLevel
+ORDER BY priceLevel;
 
 
 --Top rated restaurants in Melbourne CBD
-SELECT name, cusinetype, rating, numberOfReviews
+SELECT name, address, cuisinetype, rating, numberOfReviews
 FROM restaurant_reviews
 WHERE postcode = '3000'
 ORDER BY rating DESC
@@ -41,12 +54,12 @@ LIMIT 10;
 
 
 --Top 10 restaurants with highest no.of reviews and the highest rating
-SELECT cusinetype, name, address, MAX(numberOfReviews) AS max_reviews, MAX(avg_rating) AS max_avg_rating
+SELECT cuisinetype, name, address, MAX(numberOfReviews) AS max_reviews, MAX(avg_rating) AS max_avg_rating
 FROM (
-    SELECT cusinetype, name, address, AVG(rating) AS avg_rating, MAX(numberOfReviews) AS numberOfReviews
+    SELECT cuisinetype, name, address, AVG(rating) AS avg_rating, MAX(numberOfReviews) AS numberOfReviews
     FROM restaurant_reviews
-    GROUP BY cusinetype, name, address
+    GROUP BY cuisinetype, name, address
 ) AS subquery
-GROUP BY cusinetype, name, address
+GROUP BY cuisinetype, name, address
 ORDER BY max_reviews DESC, max_avg_rating DESC
 LIMIT 10;
